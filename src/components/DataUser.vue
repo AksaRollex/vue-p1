279615476c341eba7e0f58c0166c33def929e9bf
Ramsi Mulya W <ramsi.wicaksana@gmail.com>
2024-02-19 22:51:46 +0700

    delete success

--- src/components/DataUser.vue
+++ src/components/DataUser.vue
@@ -1,43 +1,52 @@
 <template>
-  
   <div class="">
     <HeaderAdmin />
     <div class="container mt-3">
       <table class="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Role</th>
-          <th scope="col">Aksi</th>
-        </tr>
-      </thead>
-      <tbody v-for="users in user" :key="users.id">
-        <tr class="table-secondary">
-          <td scope="row">{{ users.id }}</td>
-          <td scope="row">{{ users.name }}</td>
-          <td scope="row">{{ users.email }}</td>
-          <td scope="row">{{ users.role_id }}</td>
-          <td>
-          <router-link :to="{name:  'EditData', params: {id: users.id}}" class="btn btn-primary btn-sm">Update</router-link>
-            <button type="button" class="btn btn-danger btn-sm">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Role</th>
+            <th scope="col">Aksi</th>
+          </tr>
+        </thead>
+        <tbody v-for="users in user" :key="users.id">
+          <tr class="table-secondary">
+            <td scope="row">{{ users.id }}</td>
+            <td scope="row">{{ users.name }}</td>
+            <td scope="row">{{ users.email }}</td>
+            <td scope="row">{{ users.role_id }}</td>
+            <td>
+              <router-link
+                :to="{ name: 'EditData', params: { id: users.id } }"
+                class="btn btn-primary btn-sm"
+                >Update</router-link
+              >
+              <button
+                type="button"
+                class="btn btn-danger btn-sm"
+                @click.prevent="deleteUser(users.id)"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import HeaderAdmin from '@/components/HeaderAdmin.vue'
+import HeaderAdmin from "@/components/HeaderAdmin.vue";
 import axios from "axios";
 
 export default {
-  name: 'Admin',
-  components : {
-    HeaderAdmin
+  name: "Admin",
+  components: {
+    HeaderAdmin,
   },
   data() {
     return {
@@ -60,12 +69,19 @@ export default {
           console.log(error);
         });
     },
-    mounted() {
-      console.log("User List Mounted...");
+    async deleteUser(id) {
+      let url = `http://127.0.0.1:8000/api/delete/${id}`;
+      await axios.delete(url).then((response) => {
+        if (response.data.code == 200) {
+          alert(response.data.message);
+          this.getUser();
+        }
+      });
     },
   },
+  mounted() {
+    console.log("User List Mounted...");
+  },
 };
-
-
-
 </script>
+
